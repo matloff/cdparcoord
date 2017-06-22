734ebfb83bac7dfdd4276e58c3ac383ceb3df991
Vincent Yang <VincentYang2014@gmail.com>
2017-06-22 00:05:43 -0700

    Added much more timetests; lattice, ggparcoord/ggally, freqparcoord added to timetests

--- R/timetest.R
+++ R/timetest.R
@@ -3,6 +3,12 @@ library(pracma)
 # This program investigates runtimes of various parallel coordinates packages.
 # List of parallel coordinates packages:
 
+# freqparcoord.cd
+# freqparcoord
+# ggparcoord unavailable for R version 3.4
+# parcoord unavailable for R version 3.4
+# rggobi unavailable due to RGtk2 unavailablity (RGtk2 doesn't work either)
+
 # Load a library 10 times and return the avg time it takes
 # Timing code: https://stats.idre.ucla.edu/r/faq/how-can-i-time-my-code/
 test_load <- function(package_name){
@@ -17,29 +23,6 @@ test_load <- function(package_name){
     total_time/num_tests
 }
 
-# Plot a data set given an input dataset and a library
-timed_plot <- function(dataset, package_name){
-    if (strcmp("freqparcoord.cd", package_name) == 0){
-        # plot with freqparcoord.cd
-        start_time <- proc.time()
-        pna <- partialNA(dataset)
-        discparcoord(pna)
-        print(proc.time() - start_time)
-    }
-    else {
-        print("Unavailable package")
-    }
-}
-
-# Run a set of small datasets on all plotting packages
-time_small_datasets <- function(){
-    print("Freqparcoord.cd hrdata")
-    timed_plot("freqparcoord.cd", hrdata)
-
-    print("Freqparcoord hrdata")
-    timed_plot("freqparcoord", hrdata)
-}
-
 
 print("Freqparcoord time: ")
 test_load("freqparcoord")
@@ -50,16 +33,75 @@ test_load("freqparcoord.cd")
 print("ggparallel time: ")
 test_load("ggparallel")
 
-# ggparcoord unavailable for R version 3.4
-# parcoord unavailable for R version 3.4
-# rggobi unavailable due to RGtk2 unavailablity (RGtk2 doesn't work either)
-
+######### Plotting Tests
 ######### Test data sets #######
 # Load all data sets and libraries
 library(freqparcoord.cd)
 library(freqparcoord)
 library(ggparallel)
+library(lattice)
 data(hrdata)
 
-discparcoord(hrdata)
-time_small_datasets()
+
+# Plot a data set given an input dataset and a library
+timed_plot <- function(package_name, dataset){
+    if (strcmp("freqparcoord.cd", package_name)){
+        # plot with freqparcoord.cd
+        start_time <- proc.time()
+        pna <- partialNA(dataset)
+        discparcoord(pna)
+        print(proc.time() - start_time)
+    }
+    else if(strcmp("ggparallel", package_name)){
+        start_time <- proc.time()
+        #dataset <- head(dataset) # Displays weird/incorrectly unless we do this
+        ggparallel(colnames(dataset), data=dataset)
+        print(proc.time() - start_time)
+    }
+    else if (strcmp("freqparcoord", package_name)){
+        start_time <- proc.time()
+        freqparcoord(dataset, m=nrow(dataset))
+        print(proc.time() - start_time)
+    }
+    else if (strcmp("lattice", package_name)){
+        start_time <- proc.time()
+        parallelplot(dataset)
+        print(proc.time() - start_time)
+    }
+    else {
+        print("Unavailable package")
+    }
+}
+
+# Run a set of small datasets on all plotting packages
+time_datasets <- function(dataset, isNumerical=FALSE){
+    print("Freqparcoord.cd ")
+    timed_plot("freqparcoord.cd", dataset)
+
+    if (isNumerical){
+        print("Freqparcoord ")
+        timed_plot("freqparcoord", dataset)
+    }
+
+    print("lattice")
+    timed_plot("lattice", dataset)
+
+    print("ggparallel")
+    timed_plot("ggparallel", dataset)
+
+}
+
+cat("\nSmall Datasets:\n")
+data(Titanic_Passengers)
+Titanic_Passengers$X=NULL
+time_datasets(Titanic_Passengers)
+
+cat("\nMedium Dataset:\n")
+time_datasets(hrdata)
+
+data(prgeng)
+time_datasets(prgeng, isNumerical=TRUE)
+
+cat("\nLarge Dataset:\n")
+# MSD
+
