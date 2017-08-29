893f67bad3da38edeaf7eaecbc5d173366a23f99
Norm Matloff <matloff@cs.ucdavis.edu>
2017-08-28 21:48:36 -0700

    reorg R ftn placement in files

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 
-onAttach <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
    packageStartupMessage(
-      '\n\n\n\n\n*********************\n\n\n\n\n\n\nType ?quickstart for quick start \n\n\n\n\n`')
+      '\n\n\n\n\n*********************\n\n\n\n\n\n\nType ?quickstart for cdparcoord quick start \n\n\n\n\n`')
 }
 
